f652ece5fd753494642fd0ee496fd288c690bfb6
Sergey Gornih <seregaland@yandex.ru>
2024-12-21 10:12:10 +0300

    fix err

--- server/routes/route.js
+++ server/routes/route.js
@@ -9,3 +9,6 @@ route.post('/user/login', userController.login)
 route.get('/user/auth', authMiddleware, userController.check)
 route.get('/user/get', authMiddleware, userController.getAll)
 route.get('/user/get/:id', authMiddleware, userController.getOne)
+
+
+module.exports = route
