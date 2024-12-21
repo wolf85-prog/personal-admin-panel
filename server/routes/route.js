51b248bc7359a53d5ab6617ba8abf3dfdb3c8308
Sergey Gornih <seregaland@yandex.ru>
2024-12-21 10:10:22 +0300

    fix server

---
+++ server/routes/route.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+const route = new Router()
+const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
+
+
+route.post('/user/registration', userController.registration)
+route.post('/user/login', userController.login)
+route.get('/user/auth', authMiddleware, userController.check)
+route.get('/user/get', authMiddleware, userController.getAll)
+route.get('/user/get/:id', authMiddleware, userController.getOne)
