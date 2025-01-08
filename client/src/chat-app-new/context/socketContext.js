508d76cdb375ac88704213884c2abe584b0b7933
Sergey Gornih <seregaland@yandex.ru>
2025-01-08 10:50:53 +0300

    fix socket

--- client/src/chat-app-new/context/socketContext.js
+++ client/src/chat-app-new/context/socketContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext } from "react";
 import io from "socket.io-client";
 
-const SOCKET_URL = "https://proj.uley.team:9000"
+const SOCKET_URL = process.env.REACT_APP_HOST_SOCKET
 
 const socket = io.connect(SOCKET_URL);
 
